fa22cb1b4ae7ddfe4196b3d4c5a1f16bfcc003ba
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-11 00:10:42 +0900

    Add button click test

--- src/test/scala/tutorial/webapp/TutorialTest.scala
+++ src/test/scala/tutorial/webapp/TutorialTest.scala
@@ -1,5 +1,6 @@
 package tutorial.webapp
 
+import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.ext._
 import utest._
@@ -16,6 +17,23 @@ object TutorialTest extends TestSuite {
           .querySelectorAll("p")
           .count(_.textContent.toString == "Hello World") == 1
       )
+    }
+
+    test("ButtonClick") {
+      def messageCount =
+        document
+          .querySelectorAll("p")
+          .count(_.textContent.toString == "You clicked the button!")
+
+      val button = document.querySelector("button").asInstanceOf[dom.html.Button]
+
+      assert(button != null && button.textContent.toString == "Click me!")
+      assert(messageCount == 0)
+
+      for (c <- 1 to 5) {
+        button.click()
+        assert(messageCount == c)
+      }
     }
   }
 }
